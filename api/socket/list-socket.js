14c622dfd7265e84ae1dcd9fc94801cf6e207e67
Thiago Coutinho <thiagocoutinhor@gmail.com>
2020-03-01 01:38:46 -0300

    Funcionaliade de listagem e criação de books

--- api/socket/list-socket.js
+++ api/socket/list-socket.js
@@ -1,11 +1,40 @@
+const Book = require('../book/book-model').Book
+
 module.exports = socket => {
     const usuario = socket.handshake.session.usuario
     console.debug(`[IO LIST - ${usuario.login}] Conectou`)
 
-    // Para receber os broadcasts de listagem do
+    // Para receber os broadcasts
     socket.join(usuario.login)
+
+    // Abre listando todos os books do usuario
+    list()
+
+    // Controle de eventos do socket
+    socket.on('list', () => list())
+
+    socket.on('create', () => {
+        Book.defaultNewName(usuario.login).then(nomePadrao => {
+            const book = {
+                name: nomePadrao,
+                commands: [],
+                owner: usuario.login
+            }
+            Book.create(book).then(() => {
+                list()
+                socket.broadcast.to(usuario.login).emit('update')
+            })
+        })
+    })
 
     socket.on('disconnect', () => {
         console.debug(`[IO LIST - ${usuario.login}] Disconectou`)
     })
+
+    // Funções auxiliares
+    function list() {
+        Book.find().byUser(usuario.login).then(books => {
+            socket.emit('list', books)
+        })
+    }
 }
--- web/scripts/book-list.js
+++ web/scripts/book-list.js
@@ -1,3 +1,35 @@
 function init() {
+    io.on('list', books => {
+        montaLista(books)
+    })
 
+    io.on('update', () => {
+        io.emit('list')
+    })
+}
+
+function montaLista(books) {
+    const bookRows = d3.select('table.lista tbody')
+        .selectAll('tr')
+        .data(books)
+
+    bookRows.select('td:nth-child(1)')
+        .text(d => d.name)
+
+    bookRows.select('td:nth-child(2)')
+        .text(d => d.owner)
+
+    bookRows.exit().remove()
+
+    const novaLinha = bookRows.enter().append('tr')
+
+    novaLinha.append('td')
+        .text(d => d.name)
+
+    novaLinha.append('td')
+        .text(d => d.owner)
+}
+
+function createNewBook() {
+    io.emit('create')
 }
