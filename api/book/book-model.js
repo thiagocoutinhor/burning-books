b691ce7181861efbd449b1a2ec866b31b7608c25
Thiago Coutinho <thiagocoutinhor@gmail.com>
2020-03-01 01:38:25 -0300

    Expande o modelo do book com utilitarios

--- api/book/book-model.js
+++ api/book/book-model.js
@@ -1,11 +1,32 @@
 const mongoose = require('mongoose')
+const newName = 'Novo Book'
 
 const schema = new mongoose.Schema({
-    name: String,
+    name: { type: String, required: true, unique: true },
     commands: [String],
-    owner: String,
-    permissions: [String]
+    owner: { type: String, required: true },
+    sharedWith: [String]
 })
+
+// Monta o nome padrão do novo book
+schema.statics.defaultNewName = function(login) {
+    return this.find()
+        .byUser(login)
+        .where({name: { $regex: new RegExp(newName) }})
+        .exec()
+        .then(books => {
+            const lista = books.map(book => book.name)
+            var numero = null
+            while (lista.includes(`${newName}${numero ? `(${numero})` : ''}`)) {
+                numero++
+            }
+            return `${newName}${numero ? `(${numero})` : ''}`
+        })
+}
+
+schema.query.byUser = function(login) {
+    return this.where({$or: [{ owner: login }, { sharedWith: login }]})
+}
 
 const model = mongoose.model('Book', schema)
 
