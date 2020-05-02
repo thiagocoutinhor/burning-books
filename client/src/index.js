9be0edc8d02bd25c20b4d8a5936a0ba609757de1
Thiago Coutinho <thiagocoutinhor@gmail.com>
2020-05-02 00:53:53 -0300

    Linting

--- client/src/index.js
+++ client/src/index.js
@@ -9,11 +9,11 @@ import { App } from './app/App'
 library.add(fas)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
