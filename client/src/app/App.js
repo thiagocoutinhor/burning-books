dd98ed4035d0ca55b223d815e50c84054a7213dc
Thiago Coutinho <thiagocoutinhor@gmail.com>
2020-05-01 23:25:32 -0300

    Lint and proptypes

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,68 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 import { Login } from '../login/Login'
 import { BookList } from '../book-list/BookList'
 import { BookEditor } from '../book-editor/BookEditor'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Spinner } from 'react-bootstrap';
+import { Spinner } from 'react-bootstrap'
+import PropTypes from 'prop-types'
 
 // TODO recieve socket logoff command
 
-export function App(props) {
-  const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
+///////////////////////////////////////////////////////////////////////////////
+// App root component
+///////////////////////////////////////////////////////////////////////////////
+export function App() {
+    const [loading, setLoading] = useState(true)
+    const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    fetch('/api/login')
-      .then(response => response.status === 200 ? response.text() : null)
-      .then(user => {
-        setUser(user)
-        setLoading(false)
-      })
-  }, [])
+    useEffect(() => {
+        fetch('/api/login')
+            .then(response => response.status === 200 ? response.text() : null)
+            .then(user => {
+                setUser(user)
+                setLoading(false)
+            })
+    }, [])
 
-  const logoff = () => {
-    fetch('/api/login', { method: 'DELETE' })
-    setUser(null)
-  }
+    const logoff = () => {
+        fetch('/api/login', { method: 'DELETE' })
+        setUser(null)
+    }
 
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/book/:bookId">
-          <BookEditor />
-        </Route>
-        <Route path="/">
-          <LoadingHome loading={loading}>
-            <Home user={ user } logoff={ logoff } />
-          </LoadingHome>
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/book/:bookId">
+                    <BookEditor />
+                </Route>
+                <Route path="/">
+                    <LoadingHome loading={loading}>
+                        <Home user={ user } logoff={ logoff } />
+                    </LoadingHome>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
 }
 
-export function LoadingHome(props) {
-  return (
-    <>
-      { props.loading ? (
-        <div style={{ color: 'white', height: '100vh' }} className="d-flex flex-column justify-content-center">
-          <div className="mx-auto text-center" style={{ width: '500px'}}>
-            <Spinner animation="border" style={{ width: '10vh', height: '10vh' }}/>
-            <div className="">Loading...</div>
-          </div>
-        </div>
-      ) : (
-        props.children
-      )}
-    </>
-  )
+///////////////////////////////////////////////////////////////////////////////
+// Loading component, show an spinner while loading
+///////////////////////////////////////////////////////////////////////////////
+LoadingHome.propTypes = {
+    loading: PropTypes.bool.isRequired,
+    children: PropTypes.arrayOf(PropTypes.element)
+}
+export function LoadingHome({ loading, children }) {
+    return (
+        <>
+            { loading ? (
+                <div style={{ color: 'white', height: '100vh' }} className="d-flex flex-column justify-content-center">
+                    <div className="mx-auto text-center" style={{ width: '500px'}}>
+                        <Spinner animation="border" style={{ width: '10vh', height: '10vh' }}/>
+                        <div className="">Loading...</div>
+                    </div>
+                </div>
+            ) : (
+                children
+            )}
+        </>
+    )
 }
 
-function Home(props) {
- if (props.user) {
-    return <BookList logoff={ props.logoff } />
-  } else {
-    return <Login />
-  }
+///////////////////////////////////////////////////////////////////////////////
+// Show the book list or the login page, depending on the login status
+///////////////////////////////////////////////////////////////////////////////
+Home.propTypes = {
+    user: PropTypes.object,
+    logoff: PropTypes.func.isRequired
+}
+function Home({ user, logoff }) {
+    return user ? (<BookList logoff={ logoff } />) : (<Login />)
 }
