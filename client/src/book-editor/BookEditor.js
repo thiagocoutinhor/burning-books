074163adb5e166a1d0820a5226cc3502bc629335
Thiago Coutinho <thiagocoutinhor@gmail.com>
2020-05-08 22:56:27 -0300

    Separate file for the chunk result

--- client/src/book-editor/BookEditor.js
+++ client/src/book-editor/BookEditor.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect, useRef, useContext } from 'react'
 import PropTypes from 'prop-types'
 import { useParams, useHistory } from 'react-router-dom'
-import { Dropdown, Card, Button, ProgressBar, Table } from 'react-bootstrap'
+import { Dropdown, Card, Button } from 'react-bootstrap'
 import { SimpleDropdown } from '../components/simple-dropdown/SimpleDropdown'
 import io from 'socket.io-client'
 import { LoadingHome } from '../app/App'
 import AceEditor from 'react-ace'
-import moo from 'moo'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { doCopy, chunkCopytext, editablePrevent } from './helper'
 import { EditorNavbar } from './BookEditorNavbar'
+import { ChunkResult } from './ChunkResult'
 import './BookEditor.css'
 
 // Ace editor imports
@@ -367,139 +367,8 @@ function CommandChunk({ index, chunk, bookSocket}) {
                         </Button>
                     </Card.Footer>
                 </Card>
-                { result ? <CommandResult result={result} status={status}/> : null }
+                { result ? <ChunkResult result={result} status={status}/> : null }
             </div>
-        </div>
-    )
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// Chunk result
-///////////////////////////////////////////////////////////////////////////////
-const resultGrammar = {
-    // Extracts the tables
-    tableLine: /\+[-+]+\+/,
-    tableRow: {
-        match: /\|.*\|/,
-        value: texto => {
-            const columns = texto.split('|')
-            return columns.slice(1, columns.length - 1)
-                .map(coluna => coluna.trim())
-        }
-    },
-    // Extracts the progress bar
-    progress: {
-        match: /\[Stage \d+:?=*>?\s*\(\d+\s\+\s\d+\)\s?\/\s?\d+\]/,
-        value: text => {
-            const numbers = /\(\d+\s?\+\s?\d+\)\s?\/\s?\d+/.exec(text)[0]
-            // const actual = numbers.split('+')[0].replace('(', '').trim()
-            const actual = eval(numbers.split('/')[0])
-            const total = numbers.split('/')[1].trim()
-            return {
-                id: /Stage \d+/.exec(text)[0],
-                numbers: numbers,
-                progress: parseInt(actual) / parseInt(total) * 100
-            }
-        }
-    },
-    anything: /.+/,
-    newLine: { match: /[\n\r]/, lineBreaks: true },
-    error: moo.error
-}
-
-CommandResult.propTypes = {
-    result: PropTypes.string,
-    status: PropTypes.object
-}
-function CommandResult({ result, status }) {
-    const [bars, setBars] = useState({})
-    const [text, setText] = useState('')
-    const [running, setRunning] = useState(true)
-    const [tables, setTables] = useState([])
-
-    // TODO Text not visible when the bar is too small
-
-    // Tokenizing the result
-    // Looking for progress bars and tables
-    useEffect(() => {
-        const lexer = moo.compile(resultGrammar)
-
-        let extractedText = ''
-        lexer.reset(result)
-
-        let table = null
-        const newTables = []
-        const newBars = { ...bars }
-        Array.from(lexer).forEach(token => {
-            if (token.type === 'progress') {
-                newBars[token.value.id] = token.value
-            } else if (token.type === 'tableLine') {
-                if (!table) {
-                    table = { head: [] }
-                    newTables.push(table)
-                } else if (table.head && !table.body) {
-                    table.body = []
-                } else {
-                    table = null
-                }
-            } else if (token.type === 'tableRow') {
-                if (table && table.body) {
-                    table.body.push(token.value)
-                } else if (table && table.head) {
-                    table.head.push(token.value)
-                }
-            } else {
-                extractedText += token.text
-            }
-        })
-
-        setBars(newBars)
-        setTables(newTables)
-        setText(extractedText)
-    }, [result])
-
-    useEffect(() => {
-        if (status.executed) {
-            setRunning(false)
-        }
-    }, [status])
-
-    const makeRows = (rows) => {
-        if (rows) {
-            return rows.map((row, index) => (
-                <tr key={index}>
-                    {row.map((column, index) => <td key={index}>{column}</td>)}
-                </tr>
-            ))
-        } else {
-            return null
-        }
-    }
-
-    return (
-        <div className="result-card mr-3 ml-3">
-            { Object.values(bars).map(bar => (
-                <ProgressBar
-                    key={bar.id}
-                    striped={running}
-                    animated={running}
-                    variant={running ? 'primary' : 'success'}
-                    style={{ backgroundColor: 'rgb(231, 201, 146)' }}
-                    className="mb-1"
-                    label={`${bar.id}: ${bar.numbers}`} now={running ? bar.progress : 100}
-                />
-            )) }
-            { tables.map((table, index) => (
-                <Table key={index} striped size="sm" borderless>
-                    <thead>
-                        {makeRows(table.head)}
-                    </thead>
-                    <tbody>
-                        {makeRows(table.body)}
-                    </tbody>
-                </Table>
-            )) }
-            { text }
         </div>
     )
 }
